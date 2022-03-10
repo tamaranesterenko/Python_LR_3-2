58e0d63ed3dcacb4c36c9c59ade1ff7780ee8347
tamaranesterenko <90053368+tamaranesterenko@users.noreply.github.com>
2022-03-10 17:06:02 +0300

    Update Zadanie_2.py

--- Zadanie_2/Zadanie_2.py
+++ Zadanie_2/Zadanie_2.py
@@ -1,3 +1,8 @@
+
+# !/usr/bin/env python3
+# -*- cosing: utf-8 -*-
+
+
 #Вариант №16 (14)
 #Перед публикацией текста или документа обычно принято удалять или изменять в них
 #служебную информацию. В данном упражнении вам необходимо написать программу,
@@ -12,9 +17,6 @@
 #списке служебных слов будет присутствовать слово exam, то все следующие варианты слов
 #должны быть заменены звездочками: exam, Exam, ExaM и EXAM.
 
-# !/usr/bin/env python3
-# -*- cosing: utf-8 -*-
-
 
 if __name__ == "__main__":
     with open("text.txt", "r", encoding="utf-8") as f:
@@ -24,26 +26,24 @@ if __name__ == "__main__":
     with open("service.txt", "r", encoding="utf-8") as t:
         service = t.read().lower()
         service = service.split()
-    k = open("new.txt", "w", encoding="utf-8")
-
-    for word_ser in service:
-        word_ser = word_ser.split()
-        for word_text in text:
-            word_text = word_text.split()
-            if word_text == word_ser:
-                String = ''.join(word_text).lower()
-                long = len(String)
-                word_text = String.split()
-                while long != 0:
-                    str2 = ''
-                    for element in String:
-                        element = '*'
-                        str2 += element
-                        long -= 1
-                k.write(' ')
-                k.write(str2)
-                k.write(' ')
-            else:
-                word_text = ''.join(word_text)
-                k.write(word_text)
-    k.close()
+    with open("new.txt", "r+", encoding="utf-8") as k:
+        for word_ser in service:
+            word_ser = word_ser.split()
+            for word_text in text:
+                word_text = word_text.split()
+                if word_text == word_ser:
+                    String = ''.join(word_text).lower()
+                    long = len(String)
+                    word_text = String.split()
+                    while long != 0:
+                        str2 = ''
+                        for element in String:
+                            element = '*'
+                            str2 += element
+                            long -= 1
+                    k.write(' ')
+                    k.write(str2)
+                    k.write(' ')
+                else:
+                    word_text = ''.join(word_text)
+                    k.write(word_text)
